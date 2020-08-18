3b41f45bab88dea21b0153dcdf09981750a31eb3
Marc Carranza <marccarranzaferrez@gmail.com>
2020-08-19 00:46:10 +0200

    Functional filtering

--- src/App/helpers/filterGnomeData.js
+++ src/App/helpers/filterGnomeData.js
@@ -2,27 +2,34 @@ export const filterGnomeData = ({ data, filter: { age, weight, height, hairColor
     let filteredData = data
     
     if (age.length) {
-
+        filteredData = filteredData.filter(gnome => {
+            return (gnome.age >= age[0] && gnome.age <= age[1])
+        })
     }
 
     if (weight.length) {
-
+        filteredData = filteredData.filter(gnome => {
+            return (gnome.weight >= weight[0] && gnome.weight <= weight[1])
+        })
     }
 
     if (height.length) {
-
+        filteredData = filteredData.filter(gnome => {
+            return (gnome.height >= height[0] && gnome.height <= height[1])
+        })
     }
 
-    if (hairColor && hairColor.length) {
+    if (hairColor.length) {
         filteredData = filteredData.filter(gnome => {
             return hairColor.includes(gnome.hair_color)
         })
     }
 
     if (professions.length) {
-
+        filteredData = filteredData.filter(gnome => {
+            return _.intersection(gnome.professions, professions).length
+        })
     }
 
-    console.log(filteredData)
     return filteredData
 }
