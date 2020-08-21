6a9f51826a2d04614a2a8dc07cc6dc457212b1f1
Marc Carranza <marccarranzaferrez@gmail.com>
2020-08-21 15:15:16 +0200

    Removed unused Suspense

--- src/App/pages/Home/index.js
+++ src/App/pages/Home/index.js
@@ -1,18 +1,12 @@
-// Dependencies
-import { Suspense } from 'react'
-
 // Components 
 import { Header } from '../../components/Header'
 import { Directory } from '../../components/Directory'
-import { Loading } from '../../components/common/Loading'
  
 function HomeComponent() {
     return (
         <div>
             <Header />
-            {/* <Suspense fallback={<Loading />}> */}
             <Directory />
-            {/* </Suspense> */}
         </div>
     )
 }
